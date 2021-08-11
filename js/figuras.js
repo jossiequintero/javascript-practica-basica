59b2263ddf0e98f2b14c195ef62ddf7f71762905
jossiequintero <quinterojosy@gmail.com>
2021-08-11 09:46:14 -0500

    Changed to arrow functions geometric figures

--- js/figuras.js
+++ js/figuras.js
@@ -2,43 +2,31 @@
 console.group("Squares");
 //side of a square
 //Square Perimeter
-function perimeterSquare(side) {
-    return side * 4;
-}
+const perimeterSquare = (side) => side * 4;
 //Area del square
-function areaSquare(side) {
-    return side * side;
-}
+const areaSquare = (side) => side * side;
 console.groupEnd();
 
 //triangle Code
 console.group("Triangles");
 //Perimeter of the triangle
-function perimeterTriangle(side1, side2, base) {
-    return side1 + side2 + base;
-}
+const perimeterTriangle = (side1, side2, base) => side1 + side2 + base;
 
 //Area del triangle
-function areaTriangle(base, height) {
-    return (base * height) / 2;
-}
+const areaTriangle = (base, height) => (base * height) / 2;
 console.groupEnd();
 
 //Circles Code
 console.group("Circles");
 //Diameter
-function diameterCircle(radio) {
-    return radio * 2;
-}
+const diameterCircle = (radio) => radio * 2;
 // PI
 const PI = Math.PI;
 // Perimeter
-function perimeterCircle(radio) {
+const perimeterCircle = (radio) => {
     const diameter = diameterCircle(radio);
     return parseFloat((diameter * PI).toFixed(2));
-}
+};
 //Area
-function areaCircle(radio) {
-    return parseFloat(Math.pow(PI * radio, 2).toFixed(2));
-}
+const areaCircle = (radio) => parseFloat(Math.pow(PI * radio, 2).toFixed(2));
 console.groupEnd();
