fdea475a16822457816f3e942ab3679401b34405
jossiequintero <quinterojosy@gmail.com>
2021-08-10 21:20:25 -0500

    Squares Code

--- js/figuras.js
+++ js/figuras.js
@@ -1,1 +1,12 @@
-console.log("All right!!");
+//Square Code
+console.group("Squares");
+//side of a square
+const squareSide = 5;
+console.log(`The side of the square measures: ${squareSide} cm`);
+//Square Perimeter
+const perimeterSquare = squareSide * 4;
+console.log(`The perimeter of the square measures: ${perimeterSquare} cm`);
+//Area del square
+const areaSquare = squareSide * squareSide;
+console.log(`The area of the square measures: ${areaSquare} cm^2`);
+console.groupEnd();
