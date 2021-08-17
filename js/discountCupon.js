4c5acc94002d06ec4503376ed7a6393cf33445d5
jossiequintero <quinterojosy@gmail.com>
2021-08-16 22:58:23 -0500

    Discount with Coupon solution 2

--- js/discountCupon.js
+++ js/discountCupon.js
@@ -1,3 +1,5 @@
+//Solución #2: legibilidad, error first y muerte al switch
+const coupons = ["cupon_1", "cupon_2", "cupon_3", "cupon_4"];
 const calculatePriceWithDiscount = (price, discountP) => {
     const discountPrice = (discountP * price) / 100;
     const priceWithDiscount = price - discountPrice;
@@ -9,24 +11,18 @@ const calculatePriceWithDiscount = (price, discountP) => {
     });
     return priceWithDiscount;
 };
-/*// const coupons = ["cupon_1", "cupon_2", "cupon_3", "cupon_4"];*/
 const onClickBtnPriceDiscount = () => {
     const price = inputPrice.value;
     const coupon = inputCoupon.value;
     let discount;
-    switch (coupon) {
-        case "cupon_1":
-            discount = 10;
-            break;
-        case "cupon_2":
-            discount = 20;
-            break;
-        case "cupon_3":
-            discount = 30;
-            break;
-        default:
-            discount = 0;
-            break;
+    if (!coupons.includes(coupon)) {
+        alert(`The coupon ${coupon} is invalid!!`);
+    } else if (coupon === "cupon_1") {
+        discount = 10;
+    } else if (coupon === "cupon_2") {
+        discount = 20;
+    } else if (coupon === "cupon_3") {
+        discount = 30;
     }
     const result = calculatePriceWithDiscount(price, discount);
     resultP.innerText = `The result is: $${result}`;
