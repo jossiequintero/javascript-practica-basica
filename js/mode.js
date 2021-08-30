11b63e7169e2f61902f98a3c431c4bb11dc2ca86
jossiequintero <quinterojosy@gmail.com>
2021-08-30 10:15:36 -0500

    Moda function and HTML

--- js/mode.js
+++ js/mode.js
@@ -1,16 +1,25 @@
-const list = [1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 4, 0, 0, 0, 0];
+const list = [];
 
 const listCount = {};
 
-list.map((element) => {
-    if (listCount[element]) {
-        listCount[element] += 1;
-    } else {
-        listCount[element] = 1;
-    }
-});
+const getModa = (list) => {
+    list.map((element) => {
+        if (listCount[element]) listCount[element]++;
+        else listCount[element] = 1;
+    });
+    const listCountArray = Object.entries(listCount);
+    listCountArray.sort((a, b) => a[1] - b[1]);
+    return listCountArray.pop();
+};
+const onClickAddElement = () => {
+    const inputElement = document.getElementById("inputElement");
+    const listNumber = document.getElementById("list");
+    let value = inputElement.value;
+    list.push(value);
+    listNumber.innerHTML += `<li>${value}</li>`;
+};
 
-const listCountArray = Object.entries(listCount);
-listCountArray.sort((a, b) => a[1] - b[1]);
-console.log(listCountArray.pop());
-console.log(listCount);
+const onClickCalculateModa = () => {
+    let moda = getModa(list);
+    alert(`The element ${moda[0]} is the moda`);
+};
